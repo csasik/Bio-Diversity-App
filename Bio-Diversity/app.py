399908802e39b10a95e55bdc389653df2281fd69
csasik <chithra.kumar@gmail.com>
2018-11-15 19:40:39 -0800

    trying stuff

--- Bio-Diversity/app.py
+++ Bio-Diversity/app.py
@@ -8,11 +8,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-#from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 
 #################################################
 # Database Setup
